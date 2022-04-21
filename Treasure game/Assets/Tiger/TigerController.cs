c56eb996f7fc703d4bb4234a89ab08cff79878e8
etlimss <pavilionytesmilte@gmail.com>
2022-04-21 08:43:29 +0200

    merging from background branch to main

---
+++ Treasure game/Assets/Tiger/TigerController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TigerMovements : MonoBehaviour
+{
+    public float lookRadius = 10f;
+
+    void onDrawGrizmosSelected () {
+        
+    }
+    
+}
