342718ab27c3da97998acbc8d930896e90ae3082
littlelenka3 <56080011+littlelenka3@users.noreply.github.com>
2022-05-13 10:00:26 +0200

    fixing
    ending position and UI back

---
+++ Treasure game/Assets/Scripts/Winning.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Winning : MonoBehaviour
+{
+    public GameObject WinningPanel;
+    public GameObject TestingPanel;
+    
+    public PlayerInventory PlayerInventory;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((other.tag == "Player") && (PlayerInventory.NumberOfFlowers == 10))
+        {
+            WinningPanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            TestingPanel.SetActive(true);
+            Invoke("DisablePanel", 3.0f);
+        }
+        
+    }
+
+    void DisablePanel()
+    {
+        TestingPanel.SetActive(false);
+    }
+   
+}
