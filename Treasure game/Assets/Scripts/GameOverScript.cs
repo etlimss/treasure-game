58cfdc85ee671352f1105ae3cca2c12ab161ef53
littlelenka3 <56080011+littlelenka3@users.noreply.github.com>
2022-05-14 13:27:59 +0200

    GameOverUI plus adjustments with game objects

---
+++ Treasure game/Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour
+{
+    public PlayerInventory PlayerInventory;
+    public GameObject GameOverPanel;
+    public GameObject TestingPanel;
+    private PauseMenu _pauseMenu;
+    private MainMenu _mainMenu;
+    public void Home(int sceneID)
+    {
+
+        SceneManager.LoadScene(sceneID);
+    }
+    // changing after health bar implemented.
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((other.tag == "Player") && (PlayerInventory.NumberOfFlowers == 1))
+        {
+            GameOverPanel.SetActive(true);
+            Time.timeScale = 0f;
+           
+            // MainMenu mainMenu = GameObject.Find("MainMenu").GetComponent<"MainMenu">();
+
+        }
+        else
+        {
+            TestingPanel.SetActive(true);
+            Invoke("DisablePanel", 3.0f);
+        }
+        
+    }
+
+    public void OnReloadButton()
+    {
+        // _mainMenu.StartGame();
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(1);
+    }
+}
